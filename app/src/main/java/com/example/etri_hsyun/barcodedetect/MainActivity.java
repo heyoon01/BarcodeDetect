720e01da7a990553873fea3b64d24fa6bdf52b24
heyoon01 <watery01@gmail.com>
2018-10-01 15:16:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/etri_hsyun/barcodedetect/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.etri_hsyun.barcodedetect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener()){
+            @Override
+                    public void onClick(View v){}
+        }
+    }
+}
